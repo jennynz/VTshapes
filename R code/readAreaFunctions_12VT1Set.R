0809b1b313193dcb472363a0ce411e73b7fedd6d
jennynz <jenny.s@hotmail.co.nz>
2016-08-22 21:14:50 +1200

    Add VT01 hood.txt back into analysis

--- R code/readAreaFunctions_12VT1Set.R
+++ R code/readAreaFunctions_12VT1Set.R
@@ -3,7 +3,6 @@
 # Area functions (cross-sectional area vs. distance from lips)are read into R and interpolated with the same number of points as the original matrix.
 # Specific to the current file structure in project directory.
 # Assumes one set only. See the original file plottingDanielsMRIdata.txt to use both sets.
-# N.B. VT01 is missing data for hood. A dummy txt file named hood.txt has been created, with 29 x 2 table of N/As
 
 # Adapted by Jenny Sahng
 # 05/08/2016
@@ -27,7 +26,7 @@ for(i in 1:numVowels) {
 	vowelNames[i]<- unlist(strsplit(areaFiles[i],"\\."))[1] # Remove .txt
 }
 
-compileMRIAreas<-function(spk="VT01",interpN=FALSE)
+compileMRIAreas<-function(spk,interpN=FALSE)
 {
 	# Can decide how many values to interpolate over, or leave it with the default which is 29 (the number of data points in the area functions/numrows in .txt files)
 
@@ -45,23 +44,8 @@ compileMRIAreas<-function(spk="VT01",interpN=FALSE)
 		else { n <- nrow(datfile) }
 
 		# Need to linearly interpolate data, as different distance step in oral region than pharyngeal region.
-		if ( (spk != "VT01") && (areaFiles[i] != "hood.txt") ) {
-			LinDatfil <- approx(datfile[,1],datfile[,2],n=n)
-			alldat[i,] <- LinDatfil$y
-		} else {
-			# Coerce the list into a double type so it's the same of the output from LinDatfil$y
-			y <- as.numeric(unlist(datfile))
-			# Extend the vector of NAs for VT01 "hood.txt" to fit in combined data frame
-			if (length(y) < n) {
-				# Fortunately, R fills in empty slots on a list with NA's anyway!
-				y[length(y)+(n-length(y))] = NA
-			} else if (length(y) > n) {
-				y <- y[1:n]
-			}
-
-			# Add to alldat
-			alldat[i,] <- y
-		}
+		LinDatfil <- approx(datfile[,1],datfile[,2],n=n)
+		alldat[i,] <- LinDatfil$y
 	}
 
 	# Create data frame with speaker labels, vowel labels, and cross-sectional areas. 
@@ -76,6 +60,3 @@ for (i in 1:numVTs)
 	allVowels.df <- compileMRIAreas(spk=VTlist[i])
 	allSpeakers.df <- rbind(allSpeakers.df, allVowels.df)
 }
-
-# Omit VT01 hood.txt since they're all NAs.
-allSpeakers.df = allSpeakers.df[-9,]
