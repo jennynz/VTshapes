feb81247440c68e1f88f8198a8885fb9e4113be5
jennynz <jenny.s@hotmail.co.nz>
2016-09-08 20:50:47 +1200

    Nicer plot of comparing interpolated area functions

--- R code/pca_1Set_combined.R
+++ R code/pca_1Set_combined.R
@@ -23,15 +23,16 @@ pca <- prcomp(~., data = allSpeakers.df[,4:30]/maxArea, scale=T)
 pca.summ <- summary(pca)
 
 # Plot of PC1 and PC2, all vowels
-eplot(pca$x[,1:2], labs=as.character(allSpeakers.df[,2]), centroid=F, formant=T, col=colpalette, doellipse = T, dopoints = T)
-title(main = "Vowel properties (12 VTs x 1 Sets)", xlab = "PC1", ylab = "PC2")
+eplot(pca$x[,1:2], labs=as.character(allSpeakers.df[,2]), centroid=F, formant=T, col=colpalette, doellipse = F, dopoints = T)
+title(main = "Vowels on PC1-PC2 planes (12 VTs x 1 Sets)", xlab = "PC1", ylab = "PC2")
+
 # Compare with old 12VT1Set plot where hood was huddled in corner.
 eplot(pca$x[,1:2], labs=as.character(allSpeakers.df[,2]), centroid=F, formant=T, col=colpalette, doellipse = F, dopoints = T, xlim = c(-6,6), ylim = c(-6, 6))
-title(main = "Vowel properties (12 VTs x 1 Sets)", xlab = "PC1", ylab = "PC2")
+title(main = "Vowels on PC1-PC2 planes (12 VTs x 1 Sets)", xlab = "PC1", ylab = "PC2")
 
 # Plot of PC1 and PC2, centroids only
-eplot(pca$x[,1:2], labs=as.character(allSpeakers.df[,2]), centroid=T, formant=T, col=colpalette, doellipse = T)
-title(main = "Centroids of combined vowel properties (12 VTs x 1 Set)", xlab = "PC1", ylab = "PC2")
+eplot(pca$x[,1:2], labs=as.character(allSpeakers.df[,2]), centroid=T, formant=T, col=colpalette, doellipse = F, xlim = c(-4,4))
+title(main = "Vowel centroids on PC1-PC2 planes (12 VTs x 1 Set)", xlab = "PC1", ylab = "PC2")
 
 # Plot of standard deviations 
 plot(pca$sdev, type="p", xlab="Principal component #", ylab="Standard deviation")
@@ -42,18 +43,16 @@ pca.var <- pca.summ$importance[2,]
 plot(pca.var, type="p", xlab="Principal component #", ylab="% Variance")
 title(main = "Proportion of variance explained by principal components (12 VTs x 1 Set)")
 
-# VT02 head: Comparing linearly interpolated area function with raw distance_area txt from MATLAB
-plot (c(0,29),c(0,300),type="n", xlab="Data point", ylab="Cross-sectional area (mm^2)")
-lines(unlist(allSpeakers.df[14,-(1:2)]), col="red")
-vt02e <- read.table(paste(path,VTlist[2],"Set1","distance_area","head.txt",sep="\\"))
-lines(vt02e[,2], col="blue")
-title(main = "VT02 'head' vowel area functions")
-legend(0, 40, c("Interpolated on R","Raw area function"), lty=c(1,1), col=c("red","blue"))
-
-# VT09 had: Comparing linearly interpolated area function with raw distance_area txt from MATLAB
-plot (c(0,29),c(0,700),type="n", xlab="Data point", ylab="Cross-sectional area (mm^2)")
-lines(unlist(allSpeakers.df[89,-(1:2)]), col="red")
-vt02e <- read.table(paste(path,VTlist[9],"Set1","distance_area","had.txt",sep="\\"))
-lines(vt02e[,2], col="blue")
-title(main = "VT09 'had' vowel area functions")
-legend(0, 100, c("Interpolated on R","Raw area function"), lty=c(1,1), col=c("red","blue"))
+# Heed (VT04 vs VT09): Comparing linearly interpolated area function with raw distance_area txt from MATLAB
+plot (c(0,29),c(0,700),type="n", xlab="Data point", ylab=expression(Cross-sectional ~ area ~ (mm^{2})), xlim=c(1,29))
+# VT04
+lines(unlist(allSpeakers.df[37,-(1:2)]), col="red", lwd = 2)
+vt02e <- read.table(paste(path,VTlist[4],"Set1","distance_area","heed.txt",sep="\\"))
+lines(vt02e[,2], col="firebrick3", lwd = 2)
+# VT09
+lines(unlist(allSpeakers.df[92,-(1:2)]), col="blue", lwd = 2)
+vt09e <- read.table(paste(path,VTlist[9],"Set1","distance_area","heed.txt",sep="\\"))
+lines(vt09e[,2], col="deepskyblue3", lwd = 2)
+# Plot details
+title(main = "Raw and linearly interpolated area functions for 'had' vowel")
+legend("topleft", bty="n", c("VT04 interpolated","VT04 raw data", "VT09 interpolated", "VT09 raw data"), lty=c(1,1), col=c("red","firebrick3","blue","deepskyblue3"))
