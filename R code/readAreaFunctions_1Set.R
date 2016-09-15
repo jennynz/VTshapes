50f1bbfb25a214d8d12ef543abd2e737f1ddd8c1
jennynz <jenny.s@hotmail.co.nz>
2016-09-15 18:44:40 +1200

    Adding all the distance_area text files

--- R code/readAreaFunctions_1Set.R
+++ R code/readAreaFunctions_1Set.R
@@ -7,6 +7,33 @@
 # Adapted by Jenny Sahng
 # 05/08/2016
 
+"compileMRIAreas" <- function(spk, interpN = FALSE)
+{
+  # Can decide how many values to interpolate over, or leave it with the default which is 29 (the number of data points in the area functions/numrows in .txt files)
+  
+  # Make data matrix and vowel names vector
+  if (interpN) { n <- interpN }
+  else { n <- nrow(read.table(paste(path,spk,"Set1","distance_area",areaFiles[1],sep="\\"))) }
+  alldat <- matrix(nrow=numVowels,ncol=n,byrow=T)
+  
+  # for each speaker, go into their directory, reading the vocal tract data for each vowel, interpolate the cross-sectional areas
+  #use file name to determine vowel name, write the cross-sectional areas - only to data matrix
+  for(i in 1:numVowels)
+  {
+    filepath <- paste(path,spk,"Set1","distance_area",areaFiles[i],sep="\\")
+    datfile <- read.table(filepath)
+    
+    # Need to linearly interpolate data, as different distance step in oral region than pharyngeal region.
+    LinDatfil <- approx(datfile[,1],datfile[,2],n=n)
+    alldat[i,] <- LinDatfil$y
+    
+  }
+  
+  # Create data frame with speaker labels, vowel labels, and cross-sectional areas. 
+  alldat.df=data.frame(spk=factor(rep(spk,numVowels)),vow=factor(vowelNames),alldat)
+  return(alldat.df)
+}
+
 "read.NZE.data" <- function(interpN = FALSE) {
   
   setwd("~/Part IV Project/R code")
@@ -23,32 +50,6 @@
   vowelNames <- vector(length=numVowels)
   for(i in 1:numVowels) {
     vowelNames[i] <- unlist(strsplit(areaFiles[i],"\\."))[1] # Remove .txt
-  }
-  
-  compileMRIAreas<-function(spk, interpN = FALSE)
-  {
-    # Can decide how many values to interpolate over, or leave it with the default which is 29 (the number of data points in the area functions/numrows in .txt files)
-    
-    # Make data matrix and vowel names vector
-    if (interpN) { n <- interpN }
-    else { n <- nrow(read.table(paste(path,spk,"Set1","distance_area",areaFiles[1],sep="\\"))) }
-    alldat <- matrix(nrow=numVowels,ncol=n,byrow=T)
-    
-    # for each speaker, go into their directory, reading the vocal tract data for each vowel, interpolate the cross-sectional areas
-    #use file name to determine vowel name, write the cross-sectional areas - only to data matrix
-    for(i in 1:numVowels)
-    {
-      filepath <- paste(path,spk,"Set1","distance_area",areaFiles[i],sep="\\")
-      datfile <- read.table(filepath)
-      
-      # Need to linearly interpolate data, as different distance step in oral region than pharyngeal region.
-      LinDatfil <- approx(datfile[,1],datfile[,2],n=n)
-      alldat[i,] <- LinDatfil$y
-    }
-    
-    # Create data frame with speaker labels, vowel labels, and cross-sectional areas. 
-    alldat.df=data.frame(spk=factor(rep(spk,numVowels)),vow=factor(vowelNames),alldat)
-    return(alldat.df)
   }
   
   allSpeakers.df <- NULL
